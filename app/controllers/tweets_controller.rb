4d3e04a460b034b1d53f575ae83ac39a6202c5f6
小菅仁士 <satoshi082015@gmail.com>
2016-04-14 15:33:16 +0900

    add mysql n+1 problem to users_controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
 	before_action :move_to_index, except: :index
 
 	def index
-      @tweets = Tweet.all.order("id DESC").page(params[:page]).per(5)
+      @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
 	end
 
 	def new
@@ -11,12 +11,19 @@ class TweetsController < ApplicationController
 
 
 	def create
-      Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+      Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+	end
+
+	def destroy
+		tweet = Tweet.find(params[:id])
+		if tweet.user_id == current_user.id
+			tweet.destroy
+		end
 	end
 
 	private
 	def tweet_params
-		params.permit(:name ,:image, :text)
+		params.permit(:image, :text)
 	end
 
 	def move_to_index
